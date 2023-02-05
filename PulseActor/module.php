11d3e138eaa7e0a4b9d02f4d665fd9454fe62a74
rbh-coder <alfred.schorn@rechbergerhof.at>
2023-02-05 16:57:38 +0100

    Fixes

--- PulseActor/module.php
+++ PulseActor/module.php
@@ -549,7 +549,7 @@ class PulseActor extends IPSModule
 		    case  self::Aktiv:
                 if ($this->IsReleased())
 			    {
-                    if ($this->GetPauseTime () > 0) $actAction =  $this->SetAction ( self::SetzePause);
+                    if ($this->GetPauseTime() > 0) $actAction =  $this->SetAction ( self::SetzePause);
                 }
                 else
                 {
@@ -600,7 +600,7 @@ class PulseActor extends IPSModule
     }
     private function StartPulseTime ()
     {
-        $pulseTime =  $this-GetPulseTime ();
+        $pulseTime =  $this-GetPulseTime();
         if ($this->ReadPropertyBoolean('Debug')) IPS_LogMessage("PulsActor.StartPulseTime",'PulseTime: '.$pulseTime);
         $this->SetTimerInterval('PAC_PulseTimer', $pulseTime );
         return $pulseTime > 0;
