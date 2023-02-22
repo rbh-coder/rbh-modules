045c0fbad03d3ec03fd81c5e8e20e17b92283eef
rbh-coder <alfred.schorn@rechbergerhof.at>
2023-02-22 23:32:45 +0100

    Fixes

--- HeatingZoneController/module.php
+++ HeatingZoneController/module.php
@@ -187,8 +187,10 @@ class HeatingZoneController extends IPSModule
             //IPS_SetEventScript($id,self::MODULE_PREFIX . '_WeekTimerAction($_IPS[\'ACTION\']);');
             //IPS_SetEventScheduleAction(($id,1,"Aus",2420837,self::MODULE_PREFIX . '_WeekTimerAction('.$this->InstanceID.',1);');
              $this->SendDebug(__FUNCTION__, 'Test ID: '.$this->InstanceID, 0);
-            IPS_SetEventScheduleAction($id,1,"Aus",2420837,"HZCTRL_WeekTimerAction('.$this->InstanceID.',1);");
-            IPS_SetEventScheduleAction($id,2,"Ein",8560364,"HZCTRL_WeekTimerAction('.$this->InstanceID.',2);");
+            IPS_SetEventScheduleAction($id,1,"",0,"");
+            IPS_SetEventScheduleAction($id,2,"",0,"");
+            IPS_SetEventScheduleAction($id,1,"Aus",2420837,self::MODULE_PREFIX . "_WeekTimerAction('$this->InstanceID',1);");
+            IPS_SetEventScheduleAction($id,2,"Ein",8560364,self::MODULE_PREFIX . "_WeekTimerAction('$this->InstanceID',2);");
             $this->SendDebug(__FUNCTION__, 'IPS_GetEvent: '.json_encode(IPS_GetEvent($id), JSON_PRETTY_PRINT), 0);
         }
 
