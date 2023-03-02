ebd32930efbcfd219d504b3532aaeb28187f1e0b
rbh-coder <alfred.schorn@rechbergerhof.at>
2023-03-02 14:33:35 +0100

    Fixes

--- HeatingZoneController/module.php
+++ HeatingZoneController/module.php
@@ -13,7 +13,7 @@
 
 declare(strict_types=1);
 
-include_once __DIR__ . './libs/RBH_ModuleFunctions.php';
+include_once __DIR__ . '../libs/RBH_ModuleFunctions.php';
 
 class HeatingZoneController extends IPSModule
 {
