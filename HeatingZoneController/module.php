25d9d1a8eeb4860005900af759ae6857622f2233
rbh-coder <alfred.schorn@rechbergerhof.at>
2023-03-09 20:28:38 +0100

    Fixes

--- HeatingZoneController/module.php
+++ HeatingZoneController/module.php
@@ -312,7 +312,7 @@ class HeatingZoneController extends IPSModule
             case self::HeatOnReduced:
                 return "Absenken ". (21.0+$this->ReadPropertyFloat('OffsetTemperature'))."°C";
             case self::HeatOnBoost:
-                return "Heizen ". (21.0+$this->ReadPropertyFloat('BoostTemperature'))."°C";
+                return "Heizen ". (21.0+$this->GetBoostTemperature())."°C";
             case self::HeatUndef:
                 return "Inaktiv";
        }
