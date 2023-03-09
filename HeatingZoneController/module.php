f51975163702cee8ea11dc721abe79cac00a5fc6
rbh-coder <112027352+rbh-coder@users.noreply.github.com>
2023-03-09 22:41:08 +0100

    Fixes

--- HeatingZoneController/module.php
+++ HeatingZoneController/module.php
@@ -234,7 +234,7 @@ class HeatingZoneController extends IPSModule
             IPS_SetEventScheduleAction($id,self::HeatOff,$this->GetHeatingStatusText(1),self::DarkBlue,self::MODULE_PREFIX . "_WeekTimerAction($this->InstanceID,1);");
             IPS_SetEventScheduleAction($id,self::HeatOn,$this->GetHeatingStatusText(2),self::Yellow,self::MODULE_PREFIX . "_WeekTimerAction($this->InstanceID,2);");
             IPS_SetEventScheduleAction($id,self::HeatOnReduced,$this->GetHeatingStatusText(3),self::DarkGreen,self::MODULE_PREFIX . "_WeekTimerAction($this->InstanceID,3);");
-            if (ReadPropertyBoolean('UseBoostMode'))
+            if ($this->ReadPropertyBoolean('UseBoostMode'))
             {
                 IPS_SetEventScheduleAction($id,self::HeatOnBoost,$this->GetHeatingStatusText(4),self::Red,self::MODULE_PREFIX . "_WeekTimerAction($this->InstanceID,4);");
             }
