b2c6e3d47f98f7222ed81553a294046a6660b38f
rbh-coder <alfred.schorn@rechbergerhof.at>
2023-03-11 09:14:31 +0100

    Fixes

--- HeatingThermostat/HeatingZoneController/helper/HKTS_autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-foreach (glob(__DIR__ . '/*.php') as $filename) {
-    if (basename($filename) != 'HKTS_autoload.php') {
-        include_once $filename;
-    }
-}
