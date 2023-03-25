ff38c2f03919675129e782fbdadad593536758c9
rbh-coder <alfred.schorn@rechbergerhof.at>
2023-03-25 12:13:48 +0100

    Fixes

--- PulseActor/module.php
+++ PulseActor/module.php
@@ -367,7 +367,7 @@ class PulseActor extends IPSModule
         $this->SendDebug(__FUNCTION__, 'Die Methode wird ausgeführt. (' . microtime(true) . ')', 0);
         $this->SendDebug(__FUNCTION__, "switchName: ".$switchName, 0);
         $this->SendDebug(__FUNCTION__, "id: ".$id, 0);
-
+        $id = $this->ReadPropertyInteger($switchName);
         if ($id>1) {
             RequestAction($id, $status);
             $this->StartSignalChecker();
